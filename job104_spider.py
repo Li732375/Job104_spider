063cbcb7b8f22315cef76670a3480e3366f976a0
Li732375 <lee732375@gmail.com>
2025-03-18 18:16:19 +0800

    1 移除非必要註解 2 新增網址，提供除錯資訊 3 更新註解說明 4 新增單行理想上限字數，調整內容版型 5 更新寫入 excel 欄位資訊形式 6 調整欄位寫入順序 7 移除可能影響搜尋結果的獨立參數 8 更新複合參數內容進行測試 9 新增處理進度百分比顯示 10 註解無篩選條件，改釋出多篩選條件 11 註解逐一爬取部分的進度符號效果 12 修正寫入印出資訊

--- job104_spider.py
+++ job104_spider.py
@@ -30,8 +30,7 @@ class Job104Spider():
         
         headers = {
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36',
-            'Referer': 'https://www.104.com.tw/jobs/search/?jobsource=index_s&mode=s&page=1',
-            #'Referer': 'https://www.104.com.tw/jobs/search/',
+            'Referer': 'https://www.104.com.tw/jobs/search/?',
         }
         
         page = 1
@@ -41,6 +40,7 @@ class Job104Spider():
             r = requests.get(url, params=params, headers=headers)
             
             if r.status_code != requests.codes.ok:
+                print(f'{url}?{params}')  # 顯示當前請求的 URL
                 print(f'職缺清單請求失敗 {r.status_code}')
                 print(f'當前 encoding: {r.encoding}')
                 print(r.text)
@@ -50,19 +50,21 @@ class Job104Spider():
             # 最後資料覆寫入指定檔案，協助除錯
             with open('job_search_list.json', 'w', encoding='utf-8') as f:
                 f.write('{' + f'encoding: {r.encoding}' + '}\n')
-                json.dump(datas, f, ensure_ascii=False, indent=4)  # 使用 indent=4 美化 JSON
+                json.dump(datas, f, ensure_ascii=False, indent=4)  # 使用 indent=4 排版
 
             total_count = datas['metadata']['pagination']['total']
             
             # 將每一頁的職缺網址代碼加入 jobs
-            jobs.extend(data['link']['job'].split('/job/')[-1] for data in datas['data'])
+            jobs.extend(
+                data['link']['job'].split('/job/')[-1] for data in datas['data'])
             
             # 如果是最後一頁或空頁，就跳出迴圈
-            if page == datas['metadata']['pagination']['lastPage'] or datas['metadata']['pagination']['lastPage'] == 0:
+            if page == datas['metadata']['pagination']['lastPage'] or \
+                datas['metadata']['pagination']['lastPage'] == 0:
                 break
             
+            print(f'篩選條件清單資料({len(jobs)})，緩衝中...')
             time.sleep(random.uniform(3, 5))
-            print(f'清單資料({len(jobs)})，緩衝中...')
 
             page += 1
         
@@ -87,8 +89,7 @@ class Job104Spider():
         job_data = r.json()['data']
         # 最後資料覆寫入指定檔案，協助除錯
         with open('job_url.json', 'w', encoding='utf-8') as f:
-            f.write('{' + f'encoding: {r.encoding}' + '}\n')
-            f.write('{' + f'https://www.104.com.tw/job/{job_id}' + '}\n')
+            f.write('{' + f'"encoding": "{r.encoding}"' + ', ' + f'"URL": "https://www.104.com.tw/job/{job_id}"' + '}\n')
             json.dump(job_data, f, ensure_ascii=False, indent=4)  # 使用 indent=4 美化 JSON
 
         salary_type = {
@@ -100,50 +101,55 @@ class Job104Spider():
         }
 
         # 職缺 (全職、兼職、長短期假日工讀..)
-        workType = '全職' if len(job_data['jobDetail']['workType']) == 0 else ''.join([item for item in job_data['jobDetail']['workType']])
+        workType = '全職' if len(job_data['jobDetail']['workType']) == 0 else ', '.join([item for item in job_data['jobDetail']['workType']])
         
         data_info = {
             '更新日期': job_data['header']['appearDate'],
             '學歷': job_data['condition']['edu'],
             '工作經驗': job_data['condition']['workExp'],
-            '薪資類型 (面議 / 有薪資)': salary_type[job_data['jobDetail']['salaryType']],
+            '工作型態': workType,  
+            '薪資類型': salary_type[job_data['jobDetail']['salaryType']],
             '最高薪資': int(job_data['jobDetail']['salaryMax']),
             '最低薪資': int(job_data['jobDetail']['salaryMin']),
-            '工作型態': workType,  
+            '工作縣市': job_data['jobDetail']['addressArea'],
+            '工作地址': job_data['jobDetail']['addressDetail'],
+            '工作時段': job_data['jobDetail']['workPeriod'],
+            '公司名稱': job_data['header']['custName'],
+            '公司產業類別': job_data['industry'],
             '職缺名稱': job_data['header']['jobName'],
             '描述': job_data['jobDetail']['jobDescription'],
-            '公司名稱': job_data['header']['custName'],
-            '工作縣市': job_data['jobDetail']['addressArea'],
-            '工作里區': job_data['jobDetail']['addressRegion'][3:],
-            '工作時段': job_data['jobDetail']['workPeriod'],
-            '職缺 104 網址': url,
-            '職缺 104 公司網址': job_data['header']['custUrl'],
-            '法定福利': ''.join(job_data['welfare']['legalTag']),
-            '其他福利': job_data['welfare']['welfare'],
+            '104 職缺網址': f'https://www.104.com.tw/job/{job_id}',
+            '法定福利': ', '.join(job_data['welfare']['legalTag']),
+            #'其他福利': job_data['welfare']['welfare'],
         }
         
         # 隨機等待 3~5 秒
-        time.sleep(random.uniform(3, 5))
+        time.sleep(random.uniform(1, 3))
 
         return data_info
 
 if __name__ == "__main__":
     job104_spider = Job104Spider()
 
-    # 獨立參數
+    # 獨立參數(單選)
     uni_filter_params = {
         's5': '0',  # 0:不需輪班 256:輪班
-        'wktm': '1',  # (休假制度) 週休二日
         'isnew': '0',  # (更新日期) 0:本日最新 3:三日內 7:一週內 14:兩週內 30:一個月內
+        'wktm': '1',
     }
     
-    # 可複合參數
+    # 可複合參數(多選)
     mul_filter_params = {
-        'area': '6001016000',  # (地區)
+        'area': '6001016001,6001016002,6001016003,6001016004,6001016005,\
+            6001016007,6001016008,6001016011,6001016024,6001016027,6001018001,\
+                6001014001,6001014002,6001014003,6001014004,6001014008,\
+                    6001014014,6001001000',  # (地區) 
         's9': '1',  # (上班時段) 日班 1, 夜班 2, 大夜班 4, 假日班 8
-        'jobexp': '1,3,5,10,99',  # (經歷要求) 1年以下 1, 1-3年 2, 3-5年 3, 5-10年 4, 10年以上 5
-        'zone': '16',  # (公司類型) 16:上市上櫃 5:外商一般 4:外商資訊
+        'jobexp': '0,1,3',  # (經歷要求) 不拘 0, 1年以下 1, 1-3年 3, 3-5年 5, 5-10年 10, 10年以上 99
         'edu': '1,2,3,4,5',  # (學歷要求) 高中職以下 1,高中職 2,專科 3,大學 4,碩士 5,博士 6
+        'jobcat': '2007001004,2007001018,2007001022,2007001020,2007001012,\
+            2002001011,2007001009,2007001010,2016001013',
+        #'wt': '1,2,4,8,16',  # 長期 1, 短期 2, 假日 4, 寒假 8, 暑假 16
     }
 
     # 解析可複合篩選條件的所有可能組合
@@ -152,12 +158,21 @@ if __name__ == "__main__":
     combinations = list(product(*values))  # 產生所有可能組合
     print('搜尋條件組合總數：', len(combinations))
     
-    """  
+    # 每個條件組合取得的職缺數
+    max_num = 100
     # 使用篩選條件，轉換成 set 合併重複職缺
     alljobs_set = set()
-    for combo in combinations:
+    for idx, combo in enumerate(combinations, start=1):  # 避免從 0 開始影響百分比計算
         filter_params = {**uni_filter_params, **dict(zip(keys, combo))}
-        total_count, jobs = job104_spider.search(max_num=10, filter_params=filter_params)
+        _, jobs = job104_spider.search(max_num=max_num, 
+                                       filter_params=filter_params)
+        
+        # 計算當前進度百分比
+        progress = (idx / len(combinations)) * 100
+        print(f"處理進度：{progress:.2f}% ({idx}/{len(combinations)})", end='\r')
+
+        time.sleep(random.uniform(0.4, 2))
+        
         alljobs_set.update(jobs)  # 用 set.update() 合併職缺 ID，去除重複
 
     print('職缺總數：', len(alljobs_set))
@@ -165,10 +180,13 @@ if __name__ == "__main__":
     # 不使用篩選條件
     total_count, jobs = job104_spider.search(100)
     alljobs_set = set(jobs)
+    """ 
 
+    """ 
     # 進度符號
     progress_symbols = ['|', '/', '-', '\\']  # 定義進度符號的順序
     progress_index = 0  # 進度符號的初始索引
+    """
 
     # 逐一取得職缺詳細資料
     job_details = []
@@ -176,17 +194,20 @@ if __name__ == "__main__":
         job_info = job104_spider.get_job(job_id)
         job_details.append(job_info)
 
+        """
         # 更新進度符號顯示（動態顯示 | / - \）
         sys.stdout.write(f'\r職缺資料({len(job_details)})，緩衝中... {progress_symbols[progress_index]}') 
         sys.stdout.flush()  # 強制刷新輸出到螢幕
-        
         # 更新進度符號索引
         progress_index = (progress_index + 1) % len(progress_symbols)
+        """
     
+    """
     print()  # 讓進度列結束後換行
+    """
 
     # 將職缺資料存入 Excel
     df = pd.DataFrame(job_details)
     df.to_excel('104jobs.xlsx', index=False)
 
-    print("職缺資料已寫入 jobs.xlsx")
+    print("職缺資料已寫入 104jobs.xlsx")
